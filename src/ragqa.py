31406eab45ad4ff1f5062168280b6fdad46e3471
mrzaizai2k <chibao24.12.1999@gmail.com>
2024-06-13 15:20:28 +0700

    train gpt routing

--- src/ragqa.py
+++ src/ragqa.py
@@ -1,9 +1,10 @@
 import sys
 sys.path.append("")
 
-from langchain.prompts import PromptTemplate
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,7 +12,7 @@ load_dotenv()
 from src.utils import *
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from src.ingest import VectorDatabase
-from langchain_openai import OpenAI
+from langchain_openai import OpenAI, ChatOpenAI
 
 class RagSystem:
     def __init__(self, data_config_path = 'config/model_config.yaml'):
@@ -44,12 +45,25 @@ class RagSystem:
 
         return prompt
 
-    def load_llm(self):
+    def load_chat_llm(self):
+        llm = ChatOpenAI(model=self.data_config.get('openai_model'),
+                        openai_api_key=self.OPENAI_API_KEY, 
+                        max_tokens = self.data_config.get('openai_max_tokens')//2, #reduce the cost
+                        temperature = self.data_config.get('openai_temperature'),
+                        )
+        return llm
         
-        llm = OpenAI(openai_api_key=self.OPENAI_API_KEY, 
-                            max_tokens = self.data_config.get('openai_max_tokens'),
-                            temperature = self.data_config.get('openai_temperature'),
-                            )
+
+    def load_llm(self):
+        if self.data_config.get('openai_model')=="gpt-3.5-turbo-instruct":
+            llm = OpenAI(model=self.data_config.get('openai_model'),
+                        openai_api_key=self.OPENAI_API_KEY, 
+                        max_tokens = self.data_config.get('openai_max_tokens'),
+                        temperature = self.data_config.get('openai_temperature'),
+                        )
+        elif self.data_config.get('openai_model') in ["gpt-4o"]: # Will add more later
+            llm=self.load_chat_llm()
+
         return llm
 
     def retrieval_qa_chain(self, vector_db):
@@ -65,7 +79,8 @@ class RagSystem:
     def qa_bot(self):
 
         vector_db = FAISS.load_local(self.data_config.get('db_faiss_path'), 
-                                    self.embeddings)
+                                    self.embeddings, 
+                                    allow_dangerous_deserialization=True)
         qa_chain = self.retrieval_qa_chain(vector_db)                            
         return qa_chain 
 
